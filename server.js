7cd39a40905e55590d4bc2401e242787dda8fc72
tmac4403 <drive911@gmail.com>
2018-02-14 18:13:27 -0600

    3443

--- server.js
+++ server.js
@@ -126,6 +126,6 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
